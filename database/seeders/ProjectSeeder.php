b24de2757f3e3b632756d224ffc61cd087857d98
matteopoliti <matteo.politi17@gmail.com>
2024-03-25 16:17:51 +0100

    Feat: add migration and seeder

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use App\Models\Project;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+
+            $newProject = new Project();
+
+            $newProject->title = $faker->sentence(3);
+            $newProject->description = $faker->text(100);
+            $newProject->project_start_date = $faker->date();
+
+            $newProject->save();
+        }
+    }
+}
